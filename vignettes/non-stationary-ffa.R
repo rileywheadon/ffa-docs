fc6258a65b50cff3d60a206e4fd34fac0c50b05e
Riley Wheadon <rileywheadon@gmail.com>
2025-07-09 14:42:16 -0600

    Deployed e91849e with MkDocs version: 1.6.1

---
+++ vignettes/non-stationary-ffa.R
@@ -0,0 +1,47 @@
+## ----include = FALSE----------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+library(ffaframework)
+csv_path <- system.file("extdata", "Application_3.1.csv", package = "ffaframework")
+df <- read.csv(csv_path)
+df <- subset(df, !is.na(max)) # Remove missing values
+
+## -----------------------------------------------------------------------------
+trend <- list(location = TRUE, scale = FALSE)
+
+## ----fig.width = 10, fig.height = 8, fig.align = "center", out.width = "100%"----
+data_decomposed <- ams_decomposition(df$max, df$year, trend)
+
+selection <- select_zstatistic(data_decomposed)
+
+print(selection$recommendation)
+
+plot_lmom_diagram(selection)
+
+## -----------------------------------------------------------------------------
+fit <- fit_maximum_likelihood(
+	df$max,
+	"GNO",
+	years = df$year,
+	trend = trend
+)
+
+print(fit$params)
+
+print(fit$mll)
+
+## ----fig.width = 10, fig.height = 8, fig.align = "center", out.width = "100%"----
+uncertainty <- uncertainty_rfpl(
+	df$max,
+	"GNO",
+	years = df$year,
+	trend = trend,
+	slice = 2025
+)
+
+plot_uncertainty(uncertainty)
+
